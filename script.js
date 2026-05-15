b975331d639a3298d1301dbdb1d0b98b52900a06
maniksahni <maniksahni5@gmail.com>
2026-05-15 14:14:42 +0530

    Replace 3D section with real reel wall

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ siteNav?.addEventListener("click", (event) => {
 });
 
 const revealTargets = document.querySelectorAll(
-  ".intro > *, .product-edit article, .section-heading, .feed-card, .custom-copy, .custom-list div, .order-band > *"
+  ".showcase-copy, .intro > *, .product-edit article, .section-heading, .feed-card, .custom-copy, .custom-list div, .order-band > *"
 );
 
 revealTargets.forEach((target, index) => {
